9bccd2f2f770c3d8d5d8b77aef98e3deb608deba
Zachary Ball <zacharyball@Zacharys-MacBook-Pro.local>
2022-08-10 17:29:59 -0700

    change deps back to latest

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import { Button as MuiButton } from '@mui/material';
 import { get } from 'lodash-es';
+import { css } from '@emotion/react';
 const Button = () => {
   console.log(get({ hi: 'hello' }, 'hi', 'nope'));
-  return <MuiButton>hello</MuiButton>;
+  return (
+    <MuiButton
+      css={css`
+        background-color: red;
+      `}
+    >
+      hello
+    </MuiButton>
+  );
 };
 
 export { Button };
